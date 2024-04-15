445120dee14050ecb7656027a11b3311a23a6f42
toofaniCoder <smartcoder500@gmail.com>
2024-04-15 15:11:20 +0530

    row numbers feature implemented

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,8 @@ function App() {
   const table = useMantineReactTable({
     columns,
     data,
+    enableRowNumbers: true,
+    rowNumberDisplayMode: "original",
     initialState: { pagination: { pageSize: 5, pageIndex: 0 } },
   });
 
