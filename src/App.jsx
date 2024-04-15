b4ecf41465f7c7e3bafe714278666fa227bc66b1
toofaniCoder <smartcoder500@gmail.com>
2024-04-15 13:31:53 +0530

    basic table\

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useMemo } from "react";
+import "@mantine/core/styles.css"; //import Mantine V7 styles needed by MRT
+import "@mantine/dates/styles.css"; //if using mantine date picker features
+import "mantine-react-table/styles.css"; //import MRT styles
+import "./App.css";
+
+import { useMantineReactTable, MantineReactTable } from "mantine-react-table";
+
+import data from "./students.json";
+
+function App() {
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "name",
+        header: "Full Name",
+      },
+      {
+        accessorKey: "email",
+        header: "E-mail Address",
+      },
+
+      {
+        accessorKey: "phone",
+        header: "Phone Number",
+      },
+      {
+        accessorKey: "standard",
+        header: "Class",
+      },
+      {
+        accessorKey: "age",
+        header: "Age",
+      },
+      {
+        accessorKey: "date_of_birth",
+        header: "DOB",
+      },
+      {
+        accessorKey: "date_of_admission",
+        header: "DOA",
+      },
+
+      {
+        accessorKey: "address.pincode",
+        header: "Zip Code",
+      },
+      {
+        accessorKey: "address.city",
+        header: "City",
+      },
+      {
+        accessorKey: "address.street",
+        header: "Street Address",
+      },
+      {
+        accessorKey: "address.state",
+        header: "State Name",
+      },
+    ],
+    []
+  );
+  const table = useMantineReactTable({
+    columns,
+    data,
+    initialState: { pagination: { pageSize: 5, pageIndex: 0 } },
+  });
+
+  return (
+    <>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "#ddd",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ width: "80%", height: "70%" }}>
+          <MantineReactTable table={table} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
